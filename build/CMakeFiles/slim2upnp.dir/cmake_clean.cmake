ede2ecea5f940efab8aff85b625a2a352b6748bd
cometdom <cometdomi@gmail.com>
2026-03-23 11:22:44 +0100

    Refactor code structure for improved readability and maintainability

---
+++ build/CMakeFiles/slim2upnp.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slim2upnp.dir/src/AudioHttpServer.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/AudioHttpServer.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/Decoder.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/Decoder.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/DsdProcessor.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/DsdProcessor.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/DsdStreamReader.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/DsdStreamReader.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/FfmpegDecoder.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/FfmpegDecoder.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/FlacDecoder.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/FlacDecoder.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/HttpStreamClient.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/HttpStreamClient.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/Mp3Decoder.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/Mp3Decoder.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/OggDecoder.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/OggDecoder.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/PcmDecoder.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/PcmDecoder.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/SlimprotoClient.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/SlimprotoClient.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/UPnPController.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/UPnPController.cpp.o.d"
+  "CMakeFiles/slim2upnp.dir/src/main.cpp.o"
+  "CMakeFiles/slim2upnp.dir/src/main.cpp.o.d"
+  "slim2upnp"
+  "slim2upnp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slim2upnp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/slim2upnp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for slim2upnp.
+# This may be replaced when dependencies are built.
